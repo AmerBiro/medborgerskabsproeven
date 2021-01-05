468af6fde90d7e6ec62984edd756332799ddcc01
AmerBiro <amer.biro@gmail.com>
2021-01-05 14:15:59 +0100

    Coding splash screen

--- app/src/main/java/com/amer/medborgerskabsprven/registration/SplashScreen.java
+++ app/src/main/java/com/amer/medborgerskabsprven/registration/SplashScreen.java
@@ -2,65 +2,81 @@ package com.amer.medborgerskabsprven.registration;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amer.medborgerskabsprven.R;
+import com.amer.medborgerskabsprven.databinding.RegistrationSplashScreenBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SplashScreen#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SplashScreen extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private @NonNull
+    RegistrationSplashScreenBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private NavController controller;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public SplashScreen() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SplashScreen.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SplashScreen newInstance(String param1, String param2) {
-        SplashScreen fragment = new SplashScreen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = RegistrationSplashScreenBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        controller = Navigation.findNavController(view);
+        binding.status.setText("Checking account...");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.registration_splash_screen, container, false);
+    public void onStart() {
+        super.onStart();
+        checkUser();
+
+
     }
+
+    public void checkUser() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (firebaseUser == null) {
+                    binding.status.setText("No account founded...");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            controller.navigate(R.id.action_splashScreen_to_registration);
+                        }
+                    }, 1500);
+                }
+                else {
+                    binding.status.setText("Logging in...");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            controller.navigate(R.id.action_splashScreen_to_questionsList);
+                        }
+                    }, 1500);
+                }
+            }
+        }, 2000);
+    }
+
+
 }

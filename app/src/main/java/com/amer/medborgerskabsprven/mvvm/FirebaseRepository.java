d6ce63a8a4163269a6270cfdc484542e95b3617a
AmerBiro <amer.biro@gmail.com>
2021-01-07 04:06:56 +0100

    DataOnChanges Updated

--- app/src/main/java/com/amer/medborgerskabsprven/mvvm/FirebaseRepository.java
+++ app/src/main/java/com/amer/medborgerskabsprven/mvvm/FirebaseRepository.java
@@ -1,17 +1,28 @@
 package com.amer.medborgerskabsprven.mvvm;
 
-import androidx.annotation.NonNull;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.amer.medborgerskabsprven.questions.QuestionsList;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Type;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 public class FirebaseRepository {
@@ -30,7 +41,7 @@ public class FirebaseRepository {
     }
 
     public void getQuestionsListData() {
-        questionsListRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+/*        questionsListRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
@@ -39,8 +50,19 @@ public class FirebaseRepository {
                     onFirestoreTaskComplete.onError(task.getException());
                 }
             }
+        });*/
+
+
+        questionsListRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                Log.d(TAG, "onEvent: " + value.getDocumentChanges().toString());
+                List<QuestionsListModel> questionsListModels = value.toObjects(QuestionsListModel.class);
+                onFirestoreTaskComplete.questionsListDataAdded(questionsListModels);
+            }
         });
     }
+
 
     public interface OnFirestoreTaskComplete {
         void questionsListDataAdded(List<QuestionsListModel> questionsListModels);
